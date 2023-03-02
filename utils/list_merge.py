791348065b34fec4e3c2963b3d5fc40c411968b9
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2023-03-02 23:40:59 +0800

    更新 list_merge.py

--- utils/list_merge.py
+++ utils/list_merge.py
@@ -119,28 +119,6 @@ class SubMerge:
         with open(readme_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             f.close()
-        # 获得当前名单及各仓库节点数量
-        thanks = []
-        repo_amount_dic = {}
-        for repo in sub_list:
-            line = ''
-            if repo['enabled'] == True:
-                id = repo['id']
-                remarks = repo['remarks']
-                repo_site = repo['site']
-
-                sub_file = f'./sub/list/{id:0>2d}.txt'
-                with open(sub_file, 'r', encoding='utf-8') as f:
-                    proxies = f.readlines()
-                    if proxies == ['Url 解析错误'] or proxies == ['订阅内容解析错误']:
-                        amount = 0
-                    else:
-                        amount = len(proxies)
-                    f.close()
-                repo_amount_dic.setdefault(id, amount)
-                line = f'- [{remarks}]({repo_site}), 节点数量: `{amount}`\n'
-            if remarks != "rzhy1/12":
-                thanks.append(line)
 
         # 所有节点打印
         for index in range(len(lines)):
@@ -155,18 +133,7 @@ class SubMerge:
 
                 lines.insert(index + 1, f'合并节点总数: `{top_amount}`\n')
                 break
-        # 节点来源打印
-        for index in range(len(lines)):
-            if lines[index] == '## 节点来源\n':
-                # 清除旧内容
-                while lines[index + 1] != '\n':
-                    lines.pop(index + 1)
-
-                for i in thanks:
-                    index += 1
-                    lines.insert(index, i)
-                break
-
+        
         # 写入 README 内容
         with open(readme_file, 'w', encoding='utf-8') as f:
             data = ''.join(lines)
