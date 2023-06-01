37a2b22a904d81f5aa4360fb340d9060d291b29b
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2023-06-01 21:08:28 +0800

    更新 list_merge.py

--- utils/list_merge.py
+++ utils/list_merge.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import json
 import os
 import re
@@ -27,11 +25,12 @@ def content_write(file, output_type):
     file.write(output_type)
     file.close()
 
+
 class SubMerge:
     def __init__(self):
         self.sc = SubConvert()
-        
-    def read_list(self, json_file, split=False):  # 将 sub_list.json Url 内容读取为列表
+
+    def read_list(self, json_file, split=False):
         with open(json_file, 'r', encoding='utf-8') as f:
             raw_list = json.load(f)
         input_list = []
@@ -44,16 +43,18 @@ class SubMerge:
                 raw_list[index]['url'] = urls
                 input_list.append(raw_list[index])
         return input_list
-        
+
     def sub_merge(self, url_list):
         content_list = []
         os_call('rm -f ./sub/list/*')
 
         for index, url_info in enumerate(url_list):
             url, ids, remarks = url_info['url'], url_info['id'], url_info['remarks']
-            content = self.sc.convert_remote(url, output_type='url', host='http://127.0.0.1:25500')
+            content = self.sc.convert_remote(
+                url, output_type='url', host='http://127.0.0.1:25500')
             if content.startswith('Url 解析错误'):
-                content = self.sc.main(self.read_list(sub_list_json)[index]['url'], input_type='url', output_type='url')
+                content = self.sc.main(self.read_list(sub_list_json)[index]['url'], input_type='url',
+                                       output_type='url')
                 if content.startswith('Url 解析错误'):
                     error_msg = 'Url 解析错误'
                     print(f'Writing error of {remarks} to {ids:0>2d}.txt')
@@ -72,16 +73,34 @@ class SubMerge:
 
             with open(f'{sub_list_path}{ids:0>2d}.txt', 'w+', encoding='utf-8') as f:
                 f.write(error_msg if 'error_msg' in locals() else content)
- 
+
         print('Merging nodes...\n')
         content_raw = ''.join(content_list)
+
         def merge(content):
             return self.sc.main(content, 'content', 'YAML', {'dup_rm_enabled': True, 'format_name_enabled': True})
+
         with concurrent.futures.ThreadPoolExecutor() as executor:
             content_yaml = list(executor.map(merge, [content_raw]))[0]
         content_write(yaml_p, content_yaml)
         print(f'Done!')
-                
+
+    def test_node_availability(self, url_list, test_url):
+        available_nodes = []
+        for url_info in url_list:
+            url, ids, remarks = url_info['url'], url_info['id'], url_info['remarks']
+            try:
+                response = requests.get(test_url, timeout=5)
+                if response.status_code == 204:
+                    available_nodes.append(url_info)
+                    print(f'Node {remarks} is available')
+                else:
+                    print(f'Node {remarks} is not available')
+            except Exception as e:
+                print(f'Error occurred while testing node {remarks}: {str(e)}')
+
+        return available_nodes
+
     def geoip_update(self, url):
         print('Downloading Country.mmdb...')
         try:
@@ -91,27 +110,27 @@ class SubMerge:
         except Exception:
             print('Failed!\n')
 
-    def readme_update(self, readme_file='./README.md', sub_list=[]):  # 更新 README 节点信息
-        print('更新 README.md 中')
+    def readme_update(self, readme_file='./README.md', sub_list=[]):
+        print('Updating README.md...')
         with open(readme_file, 'r', encoding='utf-8') as f:
             lines = f.readlines()
             f.close()
 
         # 所有节点打印
         for index in range(len(lines)):
-            if lines[index] == '## 所有节点\n':  # 目标行内容
-                lines.pop(index + 1)  # 删除节点数量
+            if lines[index] == '## 所有节点\n':
+                lines.pop(index + 1)
                 with open('./sub/sub_merge_yaml.yaml', 'r', encoding='utf-8') as f:
                     proxies = f.read()
                     proxies = proxies.split('\n- ')
                     top_amount = len(proxies) - 1
                 lines.insert(index + 1, f'合并节点总数: `{top_amount}`\n')
                 break
-        
+
         # 写入 README 内容
         with open(readme_file, 'w', encoding='utf-8') as f:
             data = ''.join(lines)
-            print('完成!\n')
+            print('Done!\n')
             f.write(data)
 
 
@@ -121,3 +140,9 @@ if __name__ == '__main__':
     sub_list_remote = sm.read_list(sub_list_json, split=True)
     sm.sub_merge(sub_list_remote)
     sm.readme_update(readme, sub_list_remote)
+
+    # 测试节点可用性
+    test_url = 'https://www.YouTube.com/generate_204'
+    available_nodes = sm.test_node_availability(sub_list_remote, test_url)
+    with open('./sub/available_nodes.json', 'w', encoding='utf-8') as f:
+        json.dump(available_nodes, f, ensure_ascii=False, indent=2)
