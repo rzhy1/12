c5949cb44d13fb9f84bdf585db8113ab82496fd7
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2023-06-04 23:58:11 +0800

    更新 list_ping.py

--- utils/list_ping.py
+++ utils/list_ping.py
@@ -1,81 +1,56 @@
-import yaml
-import threading
 import requests
+import time
+import threading
+import yaml
+import os
+
+def test_latency(node):
+    url = "https://www.youtube.com/generate_204"
+    start_time = time.time()
+    try:
+        response = requests.get(url, proxies=node["proxy"], timeout=10)
+        if response.status_code == 204:
+            end_time = time.time()
+            latency = end_time - start_time
+            node["latency"] = latency
+        else:
+            node["latency"] = -1  # Indicate failed connection
+    except:
+        node["latency"] = -1  # Indicate failed connection
 
 # 读取sub_merge_yaml.yaml文件
-with open('./sub/sub_merge_yaml.yaml', 'r', encoding='utf-8') as file:
-    config = yaml.safe_load(file)
+sub_merge_path = "./sub/sub_merge_yaml.yaml"
+with open(sub_merge_path, "r") as file:
+    data = yaml.safe_load(file)
 
-proxies = config['proxies']
+proxies = data.get("proxies", [])
 
-# 定义延迟测试的目标URL
-target_url = 'https://www.YouTube.com/generate_204'
-
-# 创建结果列表
-results = []
-
-# 定义测试函数
-def test_latency(proxy):
-    try:
-        proxies = {
-            'http': f"http://{proxy['server']}:{proxy['port']}",
-            'https': f"http://{proxy['server']}:{proxy['port']}"
-        }
-        response = requests.get(target_url, proxies=proxies, timeout=10)
-        response.raise_for_status()
-        results.append({'name': proxy['name'], 'delay': response.elapsed.total_seconds()})
-    except Exception as e:
-        print(f"Error testing {proxy['name']}: {str(e)}")
-
-# 创建线程列表
+# 多线程测试延迟
 threads = []
-
-# 启动多线程测试
 for proxy in proxies:
     thread = threading.Thread(target=test_latency, args=(proxy,))
-    thread.start()
     threads.append(thread)
+    thread.start()
 
-    # 控制线程数为100
-    if len(threads) >= 100:
-        for thread in threads:
-            thread.join()
-        threads.clear()
-
-# 等待剩余线程执行完成
+# 等待所有线程完成
 for thread in threads:
     thread.join()
 
-# 按延迟排序结果列表
-sorted_results = sorted(results, key=lambda x: x['delay'])
+# 移除连接失败的节点
+proxies = [proxy for proxy in proxies if proxy["latency"] != -1]
 
-# 构建Clash配置文件
-clash_config = {
-    'proxies': {
-        proxy['name']: {
-            'type': 'http',
-            'server': proxy['server'],
-            'port': proxy['port'],
-            'delay': result['delay'],
-            'url': target_url
-        } for proxy, result in zip(proxies, sorted_results)
-    },
-    'proxy-groups': [
-        {
-            'name': 'proxies',
-            'type': 'select',
-            'proxies': [proxy['name'] for proxy in proxies]
-        }
-    ]
-}
+# 读取config.yml文件
+config_file = "./config/config.yml"
+with open(config_file, "r") as file:
+    config = yaml.safe_load(file)
 
-# 读取当前目录下的config.yml文件作为模板
-with open('./config/config.yml', 'r', encoding='utf-8') as file:
-    template = yaml.safe_load(file)
+# 更新clash配置中的节点列表
+config["proxies"] = proxies
 
-# 更新Clash配置文件的其他字段
-clash_config.update(template)
+# 写入clash.yaml文件
+clash_config_file = "./sub/clash.yaml"
+with open(clash_config_file, "w") as file:
+    yaml.safe_dump(config, file)
 
-# 保存为clash.yaml文件
-with open('./sub/clash.yaml', 'w', encoding='utf-8') as file:
-    yaml.safe_dump(clash_config, file)
+# 打印成功消息
+print("Clash configuration file generated: sub/clash.yaml")
