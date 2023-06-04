32825c42d4db1660b9e02c3c7a2ee14b46481c8d
rzhy1 <120646685+rzhy1@users.noreply.github.com>
2023-06-04 23:22:03 +0800

    更新 list_merge.py

--- utils/list_merge.py
+++ utils/list_merge.py
@@ -1,123 +1,67 @@
-#!/usr/bin/env python3
-
-import json
-import os
-import re
 import yaml
+import threading
 import requests
 
-from list_update import UpdateUrl
-from sub_convert import SubConvert
-from cv2box.utils import os_call
-from urllib import request
-import concurrent.futures
+# 读取sub_merge_yaml.yaml文件
+with open('./sub/sub_merge_yaml.yaml', 'r', encoding='utf-8') as file:
+    config = yaml.safe_load(file)
 
-# 文件路径定义
-Eterniy = './Eternity'
-readme = './README.md'
+proxies = config['proxies']
 
-sub_list_json = './sub/sub_list.json'
-sub_merge_path = './sub/'
-sub_list_path = './sub/list/'
-yaml_p = '{}/sub_merge_yaml.yaml'.format(sub_merge_path)
+# 定义延迟测试的目标URL
+target_url = 'https://www.YouTube.com/generate_204'
 
+# 创建结果列表
+results = []
 
-def content_write(file, output_type):
-    file = open(file, 'w+', encoding='utf-8')
-    file.write(output_type)
-    file.close()
+# 定义测试函数
+def test_latency(proxy):
+    try:
+        proxies = {
+            'http': f"http://{proxy['server']}:{proxy['port']}",
+            'https': f"http://{proxy['server']}:{proxy['port']}"
+        }
+        response = requests.get(target_url, proxies=proxies, timeout=10)
+        response.raise_for_status()
+        results.append({'name': proxy['name'], 'delay': response.elapsed.total_seconds()})
+    except Exception as e:
+        print(f"Error testing {proxy['name']}: {str(e)}")
 
-class SubMerge:
-    def __init__(self):
-        self.sc = SubConvert()
-        
-    def read_list(self, json_file, split=False):  # 将 sub_list.json Url 内容读取为列表
-        with open(json_file, 'r', encoding='utf-8') as f:
-            raw_list = json.load(f)
-        input_list = []
-        for index in range(len(raw_list)):
-            if raw_list[index]['enabled']:
-                if split == False:
-                    urls = re.split('\|', raw_list[index]['url'])
-                else:
-                    urls = raw_list[index]['url']
-                raw_list[index]['url'] = urls
-                input_list.append(raw_list[index])
-        return input_list
-        
-    def sub_merge(self, url_list):
-        content_list = []
-        os_call('rm -f ./sub/list/*')
+# 创建线程列表
+threads = []
 
-        for index, url_info in enumerate(url_list):
-            url, ids, remarks = url_info['url'], url_info['id'], url_info['remarks']
-            content = self.sc.convert_remote(url, output_type='url', host='http://127.0.0.1:25500')
-            if content.startswith('Url 解析错误'):
-                content = self.sc.main(self.read_list(sub_list_json)[index]['url'], input_type='url', output_type='url')
-                if content.startswith('Url 解析错误'):
-                    error_msg = 'Url 解析错误'
-                    print(f'Writing error of {remarks} to {ids:0>2d}.txt')
-                else:
-                    content_list.append(content)
-                    print(f'Writing content of {remarks} to {ids:0>2d}.txt')
-            elif content.startswith('Url 订阅内容无法解析'):
-                error_msg = 'Url 订阅内容无法解析'
-                print(f'Writing error of {remarks} to {ids:0>2d}.txt')
-            elif content is not None:
-                content_list.append(content)
-                print(f'Writing content of {remarks} to {ids:0>2d}.txt')
-            else:
-                error_msg = 'Url 订阅内容无法解析'
-                print(f'Writing error of {remarks} to {ids:0>2d}.txt')
+# 启动多线程测试
+for proxy in proxies:
+    thread = threading.Thread(target=test_latency, args=(proxy,))
+    thread.start()
+    threads.append(thread)
 
-            with open(f'{sub_list_path}{ids:0>2d}.txt', 'w+', encoding='utf-8') as f:
-                f.write(error_msg if 'error_msg' in locals() else content)
- 
-        print('Merging nodes...\n')
-        content_raw = ''.join(content_list)
-        def merge(content):
-            return self.sc.main(content, 'content', 'YAML', {'dup_rm_enabled': True, 'format_name_enabled': True})
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            content_yaml = list(executor.map(merge, [content_raw]))[0]
-        content_write(yaml_p, content_yaml)
-        print(f'Done!')
-                
-    def geoip_update(self, url):
-        print('Downloading Country.mmdb...')
-        try:
-            url = "https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb"
-            request.urlretrieve(url, './utils/Country.mmdb')
-            print('Success!\n')
-        except Exception:
-            print('Failed!\n')
+# 等待所有线程执行完成
+for thread in threads:
+    thread.join()
 
-    def readme_update(self, readme_file='./README.md', sub_list=[]):  # 更新 README 节点信息
-        print('更新 README.md 中')
-        with open(readme_file, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-            f.close()
+# 按延迟排序结果列表
+sorted_results = sorted(results, key=lambda x: x['delay'])
 
-        # 所有节点打印
-        for index in range(len(lines)):
-            if lines[index] == '## 所有节点\n':  # 目标行内容
-                lines.pop(index + 1)  # 删除节点数量
-                with open('./sub/sub_merge_yaml.yaml', 'r', encoding='utf-8') as f:
-                    proxies = f.read()
-                    proxies = proxies.split('\n- ')
-                    top_amount = len(proxies) - 1
-                lines.insert(index + 1, f'合并节点总数: `{top_amount}`\n')
-                break
-        
-        # 写入 README 内容
-        with open(readme_file, 'w', encoding='utf-8') as f:
-            data = ''.join(lines)
-            print('完成!\n')
-            f.write(data)
+# 构建Clash配置文件
+clash_config = {
+    'proxies': [{
+        'name': result['name'],
+        'type': 'http',
+        'server': proxies[index]['server'],
+        'port': proxies[index]['port'],
+        'delay': result['delay'],
+        'url': target_url
+    } for index, result in enumerate(sorted_results)],
+    'proxy-groups': [
+        {
+            'name': 'proxies',
+            'type': 'select',
+            'proxies': [result['name'] for result in sorted_results]
+        }
+    ]
+}
 
-
-if __name__ == '__main__':
-    UpdateUrl().update_main()
-    sm = SubMerge()
-    sub_list_remote = sm.read_list(sub_list_json, split=True)
-    sm.sub_merge(sub_list_remote)
-    sm.readme_update(readme, sub_list_remote)
+# 保存为clash.yaml文件
+with open('/sub/clash.yaml', 'w', encoding='utf-8') as file:
+    yaml.safe_dump(clash_config, file)
